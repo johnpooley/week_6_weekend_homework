935c5746ed8bfd5ac09c325f1c7e5fa1f698d83a
John <user@johnpoosmacbook.home>
2020-02-16 17:34:09 +0000

    mvp finished

--- js/app.js
+++ js/app.js
@@ -4,10 +4,34 @@ document.addEventListener('DOMContentLoaded', () => {
   const handleFormSubmit = function(event){
     event.preventDefault();
     console.log('form submitted');
+
+// get info from form
+    const resultList = document.querySelector('#record-list');
+
+// create text with form info
+    const newLi = document.createElement('Li');
+    newLi.textContent = `${event.target.year.value} saw the marvelous album ${event.target.albumTitle.value} by the talented ${event.target.artistName.value} featuring the wonderful ${event.target.instrument.value}!`;
+
+// add form text to list
+    resultList.appendChild(newLi);
+
+    // reset form values
+    recordForm.reset();
   }
 
+// submit button
   const recordForm = document.querySelector('#add-new-record');
-  recordForm.addEventListener('submit', handleFormSubmit)
+  recordForm.addEventListener('submit', handleFormSubmit);
 
-  
+  // delete all button
+
+  const handleDeleteAllButton = function () {
+    const recordList = document.querySelector('#record-list');
+    while (recordList.firstChild) { recordList.removeChild(recordList.firstChild);
+    }
+  }
+
+const deleteButton = document.querySelector('#deleteAll');
+deleteButton.addEventListener('click', handleDeleteAllButton);
+
 })
